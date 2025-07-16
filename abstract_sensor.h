af4d8a8b9d906bb8c772a42043f39fdcd95b2e0d
Aly <126916098+AleynaCikrik@users.noreply.github.com>
2025-07-16 20:14:49 +0200

    fix: correct AbstractSensor interface declarations

--- abstract_sensor.h
+++ abstract_sensor.h
@@ -18,7 +18,7 @@ private:
     /**
      * The sensor's name.
      */
-    char[] name;
+    const char* name;
 
 public:
 
@@ -26,15 +26,15 @@ public:
      * Creates a sensor.
      * @param sensorName The sensor's name.
      */
-    AbstractSensor(char[] sensorName);
+    AbstractSensor(const char* sensorName);
 
-    ~AbstractSensor() = default;
+    virtual ~AbstractSensor() = default;
 
     /**
      * Retrieves the sensor's name.
      * @return The sensor's name.
      */
-    char[] getName();
+    const char* getName();
 
     /**
      * Publishes the read data from this sensor to the MQTT server.
@@ -44,7 +44,7 @@ public:
      * @param client The MQTT client
      * @param topic The MQTT topic to publish the data to
      */
-    void publishData(MqttClient client, char[] topic);
+    void publishData(MqttClient& client, const char* topic);
 
     /**
      * This method should be executed once to setup the sensor.
