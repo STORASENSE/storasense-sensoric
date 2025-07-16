1e5cbe94ad3ab67250ceb59b17936595140334e8
Aly <126916098+AleynaCikrik@users.noreply.github.com>
2025-07-16 20:18:17 +0200

    fix: changed char*

--- temperature_sensor.cpp
+++ temperature_sensor.cpp
@@ -1,24 +1,24 @@
+// temperature_sensor.cpp
 #include "temperature_sensor.h"
-
-#include "Arduino.h"
+#include <Arduino.h>
 #include <Wire.h>
 
-
-TemperatureSensor::TemperatureSensor(char[] sensorName, Adafruit_ADT7410 temperatureSensor) : AbstractSensor(sensorName), sensor(temperatureSensor) {}
-
-TemperatureSensor::~TemperatureSensor() {
-    delete sensor;
-}
+TemperatureSensor::TemperatureSensor(const char* sensorName,
+                                     Adafruit_ADT7410 temperatureSensor)
+  : AbstractSensor(sensorName),
+    sensor(temperatureSensor)
+{}
 
 bool TemperatureSensor::setup() {
     if (!sensor.begin()) {
         return false;
     }
-    delay(250); // sensor takes 250 ms to get first readings
-    sensor.setResolution(ADT7410_16BIT); // set sensor resolution to 16 bits
+    delay(250);  // Zeit für den ersten Messwert
+    sensor.setResolution(ADT7410_16BIT);
     return true;
 }
 
+// Hier die const‑Methoden‑Signatur wie in AbstractSensor
 float TemperatureSensor::readData() {
     return sensor.readTempC();
 }
