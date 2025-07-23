679e9379e3bd7ca30f27ba74e61afc6eb2a79eca
Aly <126916098+AleynaCikrik@users.noreply.github.com>
2025-07-23 18:44:58 +0200

    Update connection_config.h

--- connection_config.h
+++ connection_config.h
@@ -3,14 +3,14 @@
 
 
 // connection config for Wi-Fi
-const char* WIFI_SSID = "PaddyAndAlyYeee";
-const char* WIFI_PASSWORD = "DieEinrichtungWarEinHurensohn!";
+const char* WIFI_SSID = "ALEYNA-PC7444";
+const char* WIFI_PASSWORD = "geheim123";
 
 // connection config for MQTTT
-const char* MQTT_SERVER_IP = "broker.hivemq.com";
+const char* MQTT_SERVER_IP = "aicon.dhbw-heidenheim.de";
 const int MQTT_SERVER_PORT = 1883;
-const char* MQTT_USERNAME = "";
-const char* MQTT_PASSWORD = "";
+const char* MQTT_USERNAME = "cikrika.tin23";
+const char* MQTT_PASSWORD = "AlyGeheim123!";
 
 // MQTT topics
 const char* MQTT_TOPIC_TEMPERATURE = "sensor/temperature";
